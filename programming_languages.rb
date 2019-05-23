4ddde3fbd6481db6173cf2d91249e8d02df0d139
Reina Ewing <reina.castro3@gmail.com>
2019-05-23 18:46:29 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,9 @@ require "pry"
 def reformat_languages(languages)
   # your code here
   new_hash = {}
-  languages.each do |
+  languages.each do |style, name|
+    name.each do |specs, details|
+      
     end 
   end
 end
