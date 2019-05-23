16efc881ce2e43acbbc00b49852b273a7e56263d
Reina Ewing <reina.castro3@gmail.com>
2019-05-23 18:52:33 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -9,7 +9,7 @@ def reformat_languages(languages)
       if new_hash.has_key?(specs)
         new_hash[lang][:style] << style 
       else 
-        new_hash 
+        new_hash[lang] = type
     end 
   end
 end
