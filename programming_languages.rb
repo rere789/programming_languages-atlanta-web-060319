c8d3e756861e42fa857b9af1690afaadb5350ab8
Reina Ewing <reina.castro3@gmail.com>
2019-05-23 18:35:02 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ require "pry"
 def reformat_languages(languages)
   # your code here
   languages.each do |sub_type, name|
-      sub_type.delete(:oo)
+      sub_type.delete("oo")
     name[:ruby] = { :style => :oo}
   end
 end
